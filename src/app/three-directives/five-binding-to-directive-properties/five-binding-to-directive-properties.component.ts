9b899a09d933e277548545594d5a384fdbe85f5e
nickpolizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2023-12-07 16:07:23 +0200

    corrections

--- src/app/three-directives/five-binding-to-directive-properties/five-binding-to-directive-properties.component.ts
+++ src/app/three-directives/five-binding-to-directive-properties/five-binding-to-directive-properties.component.ts
@@ -13,33 +13,43 @@ export class FiveBindingToDirectivePropertiesComponent {
   celadon:string = '#ace1af';
   extraInstructions:boolean = false;
   response:boolean = false;
+  responseText?:string;
 
-  onSelectAmaranth() {
+  onSelectAmaranth():void {
     this.fillColour = this.amaranth;
     this.extraInstructions = true;
     this.response = false;
   }
-  onSelectGlaucous() {
+  onSelectGlaucous():void {
     this.fillColour = this.glaucous;
     this.extraInstructions = true;
     this.response = false;
   }
-  onSelectCeladon() {
+  onSelectCeladon():void {
     this.fillColour = this.celadon;
     this.extraInstructions = true;
     this.response = false;
   }
-  onIconClick() {
-    if (this.fillColour === '') return
+  onIconClick():void {
     this.extraInstructions = false;
-    this.response = true;
+    this.responseMessage()
     this.fillColour = '';
+    
   }
-  onFillClick() {
+  onFillClick():void {
     this.fillColour = '';
     this.extraInstructions = false;
     this.response = false;
-
+  }
+  responseMessage():string {
+    this.response = true;
+    return this.fillColour === '#e52b50' 
+    ? this.responseText = 'Amaranth is a reddish-rose color that is a representation of the color of the flower of the amaranth plant.' 
+    : this.fillColour === '#6082b6'
+    ? this.responseText = 'Glaucous (from Ancient Greek γλαυκός (glaukós) is used to describe the pale grey or bluish-green appearance.'
+    : this.fillColour === '#ace1af'
+    ? this.responseText = 'Celadon is a term for pottery denoting both wares glazed in the jade green celadon color originated in China.'
+    : ''
   }
 
 }
