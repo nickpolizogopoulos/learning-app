e56b69a5f4b540d115898fd951d4545eeed5fbff
nickpolizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2023-12-13 13:19:23 +0200

    minor change

--- src/app/four-services-dependency-injection/two-data-service/two-data-service.component.ts
+++ src/app/four-services-dependency-injection/two-data-service/two-data-service.component.ts
@@ -3,7 +3,17 @@ import { UserListService, UserProps } from './user-list.service';
 
 @Component({
   selector: 'app-two-data-service',
-  templateUrl: './two-data-service.component.html',
+  template: `
+    
+<h4>2. Creating a Data Service</h4>
+<p class="lead mb-3">Add new users in the list or remove any existing ones. The list is provided by a service.</p>
+<app-form-data-service></app-form-data-service>
+<hr>
+<app-list-data-service
+    *ngFor="let user of users let i = index" [newUser]="user"
+></app-list-data-service>
+
+  `,
   styles: [``],
   providers: [UserListService]
 })
