64a82d62975ccd07d73fcd91108301d61967f2d1
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-01-24 15:54:01 +0200

    TD form improved

--- src/app/six-forms/one-template-driven/one-template-driven.component.ts
+++ src/app/six-forms/one-template-driven/one-template-driven.component.ts
@@ -1,8 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-// type result = boolean | null | undefined;
-
 @Component({
   selector: 'app-one-template-driven',
   templateUrl: './one-template-driven.component.html',
@@ -12,16 +10,53 @@ export class OneTemplateDrivenComponent {
 
   @ViewChild('form') form?:NgForm;
   alertSuccess:boolean = false;
+  mailResults:boolean = false;
+
+  emailData = {
+    name: '',
+    email: '',
+    gender: '',
+    message: '',
+    check: false
+  }
+
+  putRandomMessage():void {
+    const randomMessage = `Hey! I'm reaching out to see how things are going on your end. Drop me a line and let me know you're all good!`;
+
+    this.form?.form.patchValue({
+      message: randomMessage
+    })
+  }
   
   onSubmit():void {
+    this.mailResults = true;
     console.log(this.form?.value);
-    console.log(this.form?.valid ? 'FORM VALID' : 'FORM NOT VALID');
-    console.log(this.form?.touched ? 'FORM TOUCHED' : 'FORM NOT TOUCHED');
+
+    this.emailData.name = this.form?.value.name;
+    this.emailData.email = this.form?.value.email;
+    this.emailData.gender = this.form?.value.gender;
+    this.emailData.message = this.form?.value.message;
+    this.emailData.check = this.form?.value.check;
 
     this.alertSuccess = true;
     setTimeout(() => {
       this.alertSuccess = false;
-    }, 3000)
+    }, 1600)
     
+    this.form?.reset()
+  }
+
+  fillTheForm():void {
+    this.form?.setValue({
+      name: 'Nick Polizogopoulos',
+      email: 'test@test.com',
+      gender: 'Male',
+      message: `Hey! Just wanted to check in and make sure you're doing well. Let me know!`,
+      check: true
+    })
+  }
+
+  onCloseInfo():void {
+    this.mailResults = false;
   }
 }
