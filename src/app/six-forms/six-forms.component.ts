18ea6dd8c023ca54078d3a63b9b13e119a4330e3
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-01-24 16:18:33 +0200

    added data information

--- src/app/six-forms/six-forms.component.ts
+++ src/app/six-forms/six-forms.component.ts
@@ -5,7 +5,13 @@ import { Component } from '@angular/core';
   // templateUrl: './six-forms.component.html',
   template: `
   
-    <h2><mark>Forms - 2 Lessons.</mark></h2>
+    <h2><mark>Forms - 2 Lessons.
+      <span (click)="onInfoClick()" class="lead cursor-pointer">[ click for info ]</span>
+      <span *ngIf="information" class="lead">
+        Both forms are made for demonstration purposes only. They <u>do not</u> 
+        collect data nor store it or send it somewhere via actual emails.
+      </span>
+    </mark></h2>
     <section class="container-fluid">
       <div class="row mt-4 pt-4 d-flex justify-content-around">
           <app-one-template-driven class="lessonbox col-md-6 col-lg-5 col-xxl-3"></app-one-template-driven>
@@ -17,4 +23,9 @@ import { Component } from '@angular/core';
 })
 export class SixFormsComponent {
 
+  information:boolean = false;
+
+  onInfoClick():void {
+    this.information = !this.information;
+  }
 }
