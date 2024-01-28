326ad502093b9fb8f01127157abbd7ba693f6dd4
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-01-28 17:12:56 +0200

    Clean up

--- src/app/two-components-databinding/one-passing-data-between-components/list/list.component.ts
+++ src/app/two-components-databinding/one-passing-data-between-components/list/list.component.ts
@@ -4,15 +4,31 @@ import { ItemProps } from '../one-passing-data-between-components.component';
 
 @Component({
   selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  template: `
+  
+    <div class="wishBox alert alert-dismissible align-items-center">
+      <ng-content></ng-content>
+      <button (click)="deleteItem()" type="button" class="btn-close shadow-none"></button>
+    </div>
+  
+  `,
+  styles: [`
+  
+    .wishBox {
+      margin-bottom: 14px;
+      border: 1px solid rgba(209, 209, 209, 0.25);
+      border-radius: 3px;
+      box-shadow: 4px 4px 7px #d8d8d8;
+    }
+
+  `]
 })
 export class ListComponent {
 
   // element has the 'Definite Assignment Assertion' => !
   // to tell typescript that this variable will have
   // a value at runtime.
-  @Input() listItem!:ItemProps;
+  @Input() listItem?:ItemProps;
   @Output() onDeleteItem = new EventEmitter<Function>()
 
   deleteItem():void {
