72a9368292ba403b94b0ca5b0ff9ef4ab6514202
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-02-09 23:47:25 +0200

    Homepage progress

--- src/app/home/home-carousel/home-carousel.component.ts
+++ src/app/home/home-carousel/home-carousel.component.ts
@@ -13,19 +13,19 @@ interface CarouselItems {
   
     <div class="container">
       <!-- WHEN data-bs-ride="carousel", it auto-plays, when data-bs-ride="false" it stops autoplaying -->
-      <div id="carousel" class="carousel slide" data-bs-ride="carousel" data-bs-pause="false">
+      <div #carousel id="carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-pause="false">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+        
         <div class="carousel-inner">
 
-          <div *ngFor="let item of carouselItemsHorizontal" class="carousel-item {{item.activeClass}}" data-bs-interval="2500">
+          <div *ngFor="let item of carouselItemsHorizontal" class="carousel-item {{item.activeClass}}" data-bs-interval="3000">
             <img src="{{item.imageSrc}}" class="d-block w-100" alt="{{item.imageAlt}}">
               <div class="carousel-caption d-none d-md-block">
                 <button routerLink="about" class="btn">Learn more</button>
-
               </div>
           </div>
           
@@ -40,12 +40,12 @@ interface CarouselItems {
         </button> -->
 
         <div class="carouselPlayPause">
-          <svg (click)="playClick()" *ngIf="isPlaying" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+          <svg (click)="playClick()" *ngIf="isPlaying" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-play-circle niceblue" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445"/>
           </svg>
           
-          <svg (click)="pauseClick()" *ngIf="!isPlaying" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-pause-circle" viewBox="0 0 16 16">
+          <svg (click)="pauseClick()" *ngIf="!isPlaying" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pause-circle niceblue" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
             <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0z"/>
           </svg>
@@ -64,8 +64,9 @@ interface CarouselItems {
     .carousel-item {
       height: 750px;
     }
+
     .carousel-item-horizontal {
-      height: 750px;
+      
     }
 
     .btn {
@@ -74,10 +75,12 @@ interface CarouselItems {
       padding: 7px 60px;
       margin-bottom: 30px;
     }
+
     .btn:hover {
       background-color: rgba(184, 194, 219, 0.255);
       border: 2px solid;
     }
+
     .carouselPlayPause {
       margin-left: 97%;
       position: absolute;
@@ -88,12 +91,15 @@ interface CarouselItems {
 })
 export class HomeCarouselComponent {
 
+
   dataRide:string = 'carousel';
   isPlaying:boolean = false;
+
   playClick():void {
     this.dataRide = 'carousel';
     this.isPlaying = !this.isPlaying;
   }
+
   pauseClick():void {
     this.dataRide = 'false';
     this.isPlaying = !this.isPlaying;
@@ -102,36 +108,36 @@ export class HomeCarouselComponent {
   carouselItemsHorizontal:CarouselItems[] = [
 
     {
-      imageSrc: 'https://lh3.googleusercontent.com/proxy/VfpfKLXz_W5vdw8HsEDEv2hO1ruUaKtWmSkfiScTR31RE_XL0B1xPTB3-XfO1qgo1U8eUFtbn5MYfrOwNzNermneQ2JPl5J1O03Sfhdt5905SK3s1IulR0fNxgcBtQXGVtwvEPnGRf0HW_8X-vY',
+      imageSrc: 'https://www.shutterstock.com/image-illustration/3d-render-wallpaper-abstract-color-260nw-2329074805.jpg',
       imageAlt: 'Welcome to "Learning Angular" Application banner.',
       activeClass: 'active'
     },
     {
-      imageSrc: 'https://lh3.googleusercontent.com/proxy/VfpfKLXz_W5vdw8HsEDEv2hO1ruUaKtWmSkfiScTR31RE_XL0B1xPTB3-XfO1qgo1U8eUFtbn5MYfrOwNzNermneQ2JPl5J1O03Sfhdt5905SK3s1IulR0fNxgcBtQXGVtwvEPnGRf0HW_8X-vY',
+      imageSrc: 'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg',
       imageAlt: 'Learning Angular - Sections banner.',
     },
     {
-      imageSrc: 'https://lh3.googleusercontent.com/proxy/VfpfKLXz_W5vdw8HsEDEv2hO1ruUaKtWmSkfiScTR31RE_XL0B1xPTB3-XfO1qgo1U8eUFtbn5MYfrOwNzNermneQ2JPl5J1O03Sfhdt5905SK3s1IulR0fNxgcBtQXGVtwvEPnGRf0HW_8X-vY',
+      imageSrc: 'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg',
       imageAlt: 'Learning Angular - Projects banner.',
     },
   ]
 
-  carouselItemsVertical:CarouselItems[] = [
+  // carouselItemsVertical:CarouselItems[] = [
 
-    {
-      imageSrc: 'https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg',
-      imageAlt: 'Welcome to "Learning Angular" Application banner.',
-      activeClass: 'active'
-    },
-    {
-      imageSrc: 'https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg',
-      imageAlt: 'Learning Angular - Sections banner.',
-    },
-    {
-      imageSrc: 'https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg',
-      imageAlt: 'Learning Angular - Projects banner.',
-    },
-  ]
+  //   {
+  //     imageSrc: 'https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg',
+  //     imageAlt: 'Welcome to "Learning Angular" Application banner.',
+  //     activeClass: 'active'
+  //   },
+  //   {
+  //     imageSrc: 'https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg',
+  //     imageAlt: 'Learning Angular - Sections banner.',
+  //   },
+  //   {
+  //     imageSrc: 'https://www.splashlearn.com/math-vocabulary/wp-content/uploads/2022/05/image5-888x1024.jpg',
+  //     imageAlt: 'Learning Angular - Projects banner.',
+  //   },
+  // ]
 
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -26,9 +26,9 @@ import { ExternalLinkProps, SectionsAndProjectsProps, SectionsAndProjectsService
 })
 export class NavbarComponent implements OnInit {
  
-  constructor(private sections:SectionsAndProjectsService) {
-
-  }
+  constructor(
+    private sections:SectionsAndProjectsService
+  ) { }
 
   ngOnInit():void {
     this.angularSections = this.sections.angularSections;
@@ -42,23 +42,16 @@ export class NavbarComponent implements OnInit {
     
   // NAVBAR LINKS 
   navLinks = [
-    {name: 'Home', link: '/'},
-    {name: 'About', link: '/about'},
-  ]
-  
-  // NAVBAR DROPDOWN SECTIONS
-  // AngularSections = [
-  //   { name: 'The Basics', link: '/the-basics' },
-  //   { name: 'Components & Databinding', link: '/components-databinding' },
-  //   { name: 'Directives', link: '/directives' },
-  //   { name: 'Services & Dependency Injection', link: '/services'}
-  // ]
-  // NAVBAR DROPDOWN PROJECTS
-  // angularProjects = [
-  //   { name: 'To Do Application', link: '/project-to-do-application' },
-  //   { name: 'Super Signature Maker', link: '/project-super-signature-maker' },
-  //   { name: 'My Shopping List', link: '/project-shopping-list' },
-  //   { name: 'Calculator', link: '/project-calculator' },
-  // ]
+
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'About',
+      link: '/about'
+    },
+
+  ];
   
 }
